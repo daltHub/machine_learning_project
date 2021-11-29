ddd12791586e5a400082c34c4e4a62b72f748688
Conor <conorcdalton99@gmail.com>
2021-11-29 11:59:10 +0000

    Update metrics.py

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -12,6 +12,23 @@ def test():
     print("Imported Correctly")
 
 def calculate_mse(pred, true):
+    """
+    calculates the mean squared error for a model.
+
+    Parameters
+    ----------
+    pred : array 
+        predicted feature values
+
+    true : array 
+        true feature values
+
+    Returns
+    ----------
+    average_loss : float
+        mean loss between predictions and truth
+
+    """
 
     losses = mean_squared_error(y_true=true, y_pred=pred)
     average_loss = mean(losses)
@@ -19,6 +36,22 @@ def calculate_mse(pred, true):
     return average_loss
 
 def dummy(X_features, y):
+    """
+    generates predictions using a dummy regressor
+
+    Parameters
+    ----------
+    X_features : array 
+        features
+
+    y : array
+        target features
+    Returns
+    ----------
+    predictions : array
+        mean loss between predictions and truth
+
+    """
     x = DummyRegressor(strategy="mean")
     x.fit(X_features, y)
     return x.predict(X_features)
